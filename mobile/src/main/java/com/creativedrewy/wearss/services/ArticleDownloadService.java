ebfcacdd0c34d9b518957ee09f5aa83ced702cad
Andrew <creativedrewy@gmail.com>
2015-09-19 16:21:24 -0500

    Add article download service.

---
+++ mobile/src/main/java/com/creativedrewy/wearss/services/ArticleDownloadService.java
@@ -0,0 +1,24 @@
+package com.creativedrewy.wearss.services;
+
+/**
+ *
+ */
+public class ArticleDownloadService {
+
+//    public static Observable<InputStream> downloadFile(String url) {
+//        return Observable.create(subscriber -> {
+//            Request request = new Request.Builder().url(url).build();
+//            new OkHttpClient().newCall(request).enqueue(new Callback() {
+//                @Override
+//                public void onFailure(Request request, IOException e) {
+//                    subscriber.onError(e);
+//                }
+//                @Override
+//                public void onResponse(Response response) throws IOException {
+//                    subscriber.onNext(response.body().byteStream());
+//                    subscriber.onCompleted();
+//                }
+//            });
+//        });
+//    }
+}
